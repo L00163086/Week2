908fe36f1dcc8352670eea5fa1c232fbbcd8d37d
Chetan <91426296+L00163086@users.noreply.github.com>
2021-10-04 00:04:53 +0530

    Add files via upload
    iterate_num example

---
+++ iterate_num.py
@@ -0,0 +1,17 @@
+"""
+# -----------------------------------------------
+# File        : iterate_num.py
+# Created:    : 03-10-2021 23:52
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+
+#------------------------------------------------
+
+"""
+
+if __name__ == '__main__':
+from string import ascii_lowercase:
