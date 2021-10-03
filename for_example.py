950ffcda551d43e5ad19e2e935c38fc79a42475d
Chetan <91426296+L00163086@users.noreply.github.com>
2021-10-04 00:04:25 +0530

    Add files via upload
    Loop exerise

---
+++ for_example.py
@@ -0,0 +1,23 @@
+"""
+# -----------------------------------------------
+# File        : for_example.py
+# Created:    : 03-10-2021 23:51
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+
+#------------------------------------------------
+
+"""
+if __name__ == '__main__':
+    for i in range(10,15,1):
+        print(i)
+
+print(" ")
+
+if __name__ == '__main__':
+    for i in range(4,15,3):
+        print(i)
